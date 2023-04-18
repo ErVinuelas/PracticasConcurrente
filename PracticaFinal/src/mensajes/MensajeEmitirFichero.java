1bf84658c21aa312486c42b5c334c0f6b33b429d
ErVinuelas <pablma24@ucm.es>
2023-04-18 19:51:24 +0200

    Arreglo y refinamiento de los distintos tipos de mensajes

--- PracticaFinal/src/mensajes/MensajeEmitirFichero.java
+++ PracticaFinal/src/mensajes/MensajeEmitirFichero.java
@@ -5,15 +5,20 @@ import java.io.Serializable;
 public class MensajeEmitirFichero extends Mensaje implements Serializable {
 
 	private static final long serialVersionUID = -5644148867784751333L;
-	private TipoConexion tipo;
 
-	public MensajeEmitirFichero(TipoConexion tipo, boolean ack) {
+    private String fileName;
+
+	public MensajeEmitirFichero(String fileName, boolean ack) {
 		super(TipoMensaje.EMITIR_FICHERO,ack);
-		this.tipo=tipo;
-	}
+        this.fileName = fileName;
+    }
 	
 	public TipoConexion getMessage() {
 		return tipo;
 	}
+
+    public getFileName() {
+        return fileName;
+    }
 
 }
