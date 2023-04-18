ccac1087a59aca24e2efc5f305925e6551236faa
ErVinuelas <pablma24@ucm.es>
2023-04-18 19:56:39 +0200

    Arreglando mi error

--- PracticaFinal/src/mensajes/Mensaje.java
+++ PracticaFinal/src/mensajes/Mensaje.java
@@ -1,23 +1,24 @@
 package mensajes;
+
 import java.io.Serializable;
 
 public abstract class Mensaje implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private TipoMensaje tipo;
-	private boolean ack;
-	
-	public Mensaje(TipoMensaje tipo, boolean ack) {
-		this.tipo=tipo;
-		this.ack=ack;
-	}
-	
-	public TipoMensaje getTipo() {
-		return tipo;
-	}
-	
-	public boolean isACK() {
-		return ack;
-	}
+    private static final long serialVersionUID = 1L;
+    private TipoMensaje tipo;
+    private boolean ack;
+
+    public Mensaje(TipoMensaje tipo, boolean ack) {
+        this.tipo = tipo;
+        this.ack = ack;
+    }
+
+    public TipoMensaje getTipo() {
+        return tipo;
+    }
+
+    public boolean isACK() {
+        return ack;
+    }
 
 }
--- PracticaFinal/src/mensajes/MensajePedirFichero.java
+++ PracticaFinal/src/mensajes/MensajePedirFichero.java
@@ -5,20 +5,13 @@ import java.io.Serializable;
 public class MensajePedirFichero extends Mensaje implements Serializable {
 
 	private static final long serialVersionUID = -5644148867784751333L;
-	private TipoConexion tipo;
-
     private String fileName;
 
-	public MensajePedirFichero(TipoConexion tipo, boolean ack, String fileName) {
+	public MensajePedirFichero(String fileName, boolean ack) {
 		super(TipoMensaje.PEDIR_FICHERO,ack);
-		this.tipo=tipo;
         this.fileName = fileName;
 	}
-	
-	public TipoConexion getMessage() {
-		return tipo;
-	}
-
+    
     public getFileName() {
         return fileName;
     }
