ff5121aadae0c1319e6c5f11577aa17889883936
ErVinuelas <pablma24@ucm.es>
2023-04-19 17:39:11 +0200

    Limipeza y comentado de la clase Cliente

--- PracticaFinal/src/Cliente.java
+++ PracticaFinal/src/Cliente.java
@@ -19,11 +19,26 @@ import mensajes.TipoConexion;
 public class Cliente {
 
 	protected Socket sc;
-	protected Usuario yo;
+
+	// Guarda los archivos y su dirección asociada, nosotros almacenamos
+	// directamente el string que hay
+	// asociado a un nombre de fichero.
 	public static HashMap<String, String> archivos;
+
+	// Información del usuario que hay asociado al cliente.
+	protected Usuario yo;
+
+	// Flujos asociados al cliente
 	protected ObjectOutputStream fOut;
+	// TODO: Por que no hay fIn? Hay alguna razón o es un error?
+
+	// Proceso OyenteServidor asociado
 	protected OyenteServidor hilo;
+
+	// Entrada de usuario
 	protected Scanner scan;
+
+	// Semáforo que nos permite regular el flujo de archivos.
 	protected Semaphore viaLibre;
 
 	public Cliente() {
@@ -32,23 +47,29 @@ public class Cliente {
 		viaLibre = new Semaphore(0);
 	}
 
-	public void init() throws UnknownHostException, IOException{
+	// Inicializamos el cliented
+	public void init() throws UnknownHostException, IOException {
 		String nombre, dir;
 		int port;
 
+		// Menú interactivo inicial con el usuario
 		Log.console("Introduce tu nombre");
 		nombre = scan.nextLine();
-		yo=new Usuario(nombre, "localhost", 1000);
+
+		yo = new Usuario(nombre, "localhost", 1000);
 
 		Log.console("Quieres compartir algun archibo? (s/n)");
 		String respuesta = scan.nextLine();
-		while(respuesta.equals("s")){
+
+		while (respuesta.equals("s")) {
 			Log.console("Introduce el nombre del archivo");
 			String archivo = scan.nextLine();
 			yo.addFile(archivo);
+			
 			Log.console("Introduce la ruta del archivo");
 			String ruta = scan.nextLine();
 			archivos.put(archivo, ruta);
+			
 			Log.console("Quieres compartir otro archivo? (s/n)");
 			respuesta = scan.nextLine();
 		}
@@ -59,36 +80,39 @@ public class Cliente {
 		Log.console("Introduce el puerto del servidor");
 		port = scan.nextInt();
 
+		//Inicialización del resto de elementos del cliente.
 		sc = new Socket(dir, port);
 		hilo = new OyenteServidor(sc, yo, viaLibre, this);
 		hilo.start();
 		fOut = hilo.getFout();
 	}
 
-	public void loop() throws IOException, InterruptedException{
+	public void loop() throws IOException, InterruptedException {
 		boolean sigue = true;
-		while(sigue){
-			Log.console("Que quieres hacer?\n\t 1. Descargar un archivo\n\t 2. Ver Usuarios conectados\n\t 3. Salir \n");
+		//Menú interactivo con el usuario.
+		while (sigue) {
+			Log.console(
+					"Que quieres hacer?\n\t 1. Descargar un archivo\n\t 2. Ver Usuarios conectados\n\t 3. Salir \n");
 			int respuesta = scan.nextInt();
 			scan.nextLine();
-			switch(respuesta){
-				case 1:
-					Log.console("Introduce el nombre del archivo que quieres descargar");
-					String archibo = scan.nextLine();
-					fOut.writeObject(new MensajePedirFichero(archibo, false));
-					viaLibre.acquire();
-					break;
-				case 2:
-					fOut.writeObject(new MensajeSolicListaUsuar(null, false));
-					viaLibre.acquire();
-					break;
-				case 3:
-					fOut.writeObject(new MensajeConexion(TipoConexion.CERRAR, false, yo));
-					sigue = false;
-					break;
-				default:
-					System.out.println("Opcion no valida");
-					break;
+			switch (respuesta) {
+			case 1:
+				Log.console("Introduce el nombre del archivo que quieres descargar");
+				String archibo = scan.nextLine();
+				fOut.writeObject(new MensajePedirFichero(archibo, false));
+				viaLibre.acquire();
+				break;
+			case 2:
+				fOut.writeObject(new MensajeSolicListaUsuar(null, false));
+				viaLibre.acquire();
+				break;
+			case 3:
+				fOut.writeObject(new MensajeConexion(TipoConexion.CERRAR, false, yo));
+				sigue = false;
+				break;
+			default:
+				System.out.println("Opcion no valida");
+				break;
 			}
 
 		}
